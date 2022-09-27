f76f7d4c44974dff819dd421bfeba49ebbc7b633
Jasiu Imac <jan@kaletka.com.pl>
2022-09-27 15:00:14 +0200

    Added About text

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="about">
     <div class="textAbout">
-      I am front-end developer. This site was created using VUE3 for content
-      management I used content full. For the email part emailsJS take care. If
-      you want to contact me, or you have any remarks for this website maybe any
-      ideas how to improve please use contant form!
+      <h1>
+        I am junior front-end developer.<br />
+        I started to learn web-development 1,5 year ago. If you want to contact
+        me, or you have any remarks for this website please use contant form!
+      </h1>
     </div>
     <img src="../assets/foto.png" alt="" />
   </div>
@@ -17,7 +18,17 @@
 }
 
 .textAbout {
-  width: 300px;
+  width: 350px;
+  color: var(--white);
+  background-color: var(--grey);
+  padding: 20px;
+  border-radius: 20px;
+  text-align: justify;
+}
+h1 {
+  font-weight: 500;
+
+  font-size: larger;
 }
 img {
   width: 200px;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 const firstLine: string[] = ["Hi! I'm Jasiu"];
-const secondLine: string[] = ["front-end"];
-const thirdLine: string[] = ["developer"];
+const secondLine: string[] = ['front-end'];
+const thirdLine: string[] = ['developer'];
 </script>
 
 <template>
@@ -42,16 +42,16 @@ main {
   font-weight: 200;
   font-size: 2rem;
   line-height: 95%;
-  font-family: "Alfa Slab One", cursive;
+  font-family: 'Alfa Slab One', cursive;
   text-shadow: 2px 2px 3px rgba(0, 0, 0, 0.8);
 }
 p {
   font-size: 0.8rem;
-  font-family: "Pacifico", cursive;
+  font-family: 'Pacifico', cursive;
   opacity: 0.5;
 }
 .startH1 {
-  margin-left: -20px;
+  margin-right: auto;
 }
 .endH1 {
   margin-left: auto;
